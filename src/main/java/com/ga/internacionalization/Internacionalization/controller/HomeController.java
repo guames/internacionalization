adf4ca03788efc48fbff5981d59ee84525848c48
Gustavo Ames <gustavo.ames87@gmail.com>
2018-10-31 13:05:23 -0200

    Initial Project of internacionalization

---
+++ src/main/java/com/ga/internacionalization/Internacionalization/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.ga.internacionalization.Internacionalization.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Locale;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
+
+    @GetMapping(path = "/hello")
+    public String helloInternacionalized(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("application.hello", null, locale);
+    }
+
+}
